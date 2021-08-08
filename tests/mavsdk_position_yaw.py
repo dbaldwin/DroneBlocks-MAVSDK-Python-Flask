2252a781beb73861c7582eab30535cdf2053762b
Dennis Baldwin <dennisbaldwin@gmail.com>
2021-08-07 20:47:28 -0500

    Relative yaw example

--- tests/mavsdk_position_yaw.py
+++ tests/mavsdk_position_yaw.py
@@ -22,7 +22,12 @@ async def run():
         print(f"Starting offboard mode failed with error code: {error._result.result}")
         return
 
-    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, -45))
+    # We will get the current heading and then yaw relative to that
+    relative_yaw = 45
+    heading = await get_heading(drone)
+    final_yaw = heading + relative_yaw
+
+    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, final_yaw))
 
     await asyncio.sleep(10)
 
@@ -33,6 +38,10 @@ async def run():
         return
 
     await drone.action.land()
+
+async def get_heading(drone):
+    async for euler in drone.telemetry.attitude_euler():
+        return euler.yaw_deg
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
